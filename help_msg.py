a062230307013fc6e4b52655d237d5ff27ec8db5
adam-kabbara <62020687+adam-kabbara@users.noreply.github.com>
2021-01-06 22:15:28 +0200

    added help msg in diffrent file

---
+++ help_msg.py
@@ -0,0 +1,27 @@
+help_msg = """Hello to daily math questions
+Below is a list of commands for the bot ado
+Note before typing the command please proceed it with \"ado\"
+- animal (difficulty) --> gives you a animal question
+- anime (difficulty) --> gives you a anime question
+- bg (difficulty) --> gives you a board game question
+- book (difficulty) --> gives you a book question
+- cartoon (difficulty) --> gives you a cartoon question
+- celebrity (difficulty) --> gives you a celebrity question
+- computer (difficulty) --> gives you a computer question
+- film (difficulty) --> gives you a film question
+- geography (difficulty) --> gives you a geography question
+- gk (difficulty) --> gives you a general knowledge question
+- history (difficulty) --> gives you a history question
+- math (difficulty) --> gives you a math question
+- music (difficulty) --> gives you a music question
+- tv (difficulty) --> gives you a television question
+- vehicle (difficulty) --> gives you a vehicle question
+- vg (difficulty) --> gives you a video game question
+- sport (difficulty) --> gives you a sport question
+- science (difficulty) --> gives you a science question
+- random fact ; rf --> gives you a random fact
+- random cat fact ; rcf --> gives you a random fact about cats
+- (type answer) --> check if you're answer is correct
+- reveal / show --> reveals the answer
+- help --> get help... duh
+Note: The difficulties are (easy, medium, hard)"""
