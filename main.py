7b5d2e82e0d606a6a12889728bdc6eea606bbe4e
adam-kabbara <62020687+adam-kabbara@users.noreply.github.com>
2021-02-01 02:15:48 -0500

    sends weather and give bad jokes new help command

--- main.py
+++ main.py
@@ -3,17 +3,26 @@ import discord
 import requests
 import base64
 import random
-from help_msg import help_msg
+import os
+from help_msg import help_msg, help_weather_msg
+import json
+import tqdm
+import private_data
+import aiohttp
+from io import BytesIO
 
-with open('private data.txt') as f:
-    token = f.read()
+token = private_data.ids['discord']
+weather_token = private_data.ids['weather']
 
 client = discord.Client()
-base_url = 'https://opentdb.com/api.php?amount=1&category=SUBJECTCODE&difficulty=spotholder&encode=base64&token=YOURTOKENHERE'
+base_url = 'https://opentdb.com/api.php?amount=1&encode=base64'
 data = ()
 token_url = "https://opentdb.com/api_token.php?command=request"
+weather_url = "http://api.openweathermap.org/data/2.5/weather?"
+weather_img_url = "http://openweathermap.org/img/wn/CODE@2x.png"
 res = requests.get(token_url)
 question_token = res.json()['token']
+trivia_asked = False
 
 trivia_to_letter = {'a': 0, 'b': 1, 'c': 2, 'd': 3}
 
@@ -39,21 +48,11 @@ subject_codes = {
 }
 
 
-async def keep_token_active():
-    while True:
-        await asyncio.sleep(20880)
-        requests.get(base_url)
-        print('sent msg to keep token active')
-
-
-def get_data(url, difficulty='', subject_code=''):
-    url = url.replace('YOURTOKENHERE', question_token)
-    url = url.replace('spotholder', difficulty)
-    url = url.replace('SUBJECTCODE', str(subject_code))
+def get_data(url, data={}):
     try:
-        res = requests.get(url)
+        res = requests.get(url, params=data)
     except Exception as exp:
-        return f'Error retrieving data ({exp})'
+        print(f'Error retrieving data ({exp})')
     else:
         return res.json()
 
@@ -87,41 +86,194 @@ async def send_rf(channel):
     await client.wait_until_ready()
     channel = client.get_channel(channel)
     while True:
-        data = get_data('https://useless-facts.sameerkumar.website/api')
+        data = get_data(get_random_question_url())
         print(data)
-        msg_to_send = data['data']
+        try: 
+            msg_to_send = data['data']
+        except KeyError:
+            msg_to_send = data['text']
         await channel.send(msg_to_send)
         await asyncio.sleep(86400)  # one day
 
 
+def get_random_question_url():
+    url = random.choice(['https://uselessfacts.jsph.pl/random.json?language=en', 'https://useless-facts.sameerkumar.website/api'])
+    return url
+
 print('running')
 
 
+def handel_token_error(res):
+    while res['response_code'] != 0:
+        print(f'error code {res["response_code"]}')
+        if res['response_code'] == 4:
+            try:
+                res = requests.get(f'https://opentdb.com/api_token.php?command=reset&token={question_token}').json()
+                print(res)
+            except Exception as exp:
+                return f'Error retrieving data ({exp})'
+
+        elif res['response_code'] == 3:
+            print('debug')
+            res = (requests.get(token_url)).json()
+            question_token = res['token']
+            print(res)
+            print(question_token)
+            
+    return res
+
+
+def get_city_code(city, country):
+    if country == 'usa' or country == 'us' or country == 'United states of america':
+        country = 'united states'
+    found_cities = []
+    with open('city.list.json/city.list.json') as f:
+        city_data = json.load(f)
+
+    country_code = get_country_code(country)
+    if country_code is not None:
+        for dictionary in tqdm.tqdm(city_data):
+            if dictionary['name'].lower() == city and dictionary['country'] == country_code: 
+                found_cities.append(dictionary)
+
+        return found_cities
+    else:
+        return None
+
+
+def get_country_code(country):
+    with open ('city.list.json/country_codes.json') as f:
+        country_codes = json.load(f)
+    try: 
+        country_code = country_codes[country.title()]
+    except KeyError:
+        return None
+    else:
+        return country_code
+
+
+def get_weather_msg(code):
+
+    with open ('city.list.json/country_codes.json') as f:
+        country_codes = json.load(f)
+    inv_country_codes = {v: k for k, v in country_codes.items()}
+
+    code = data['weather'][0]['icon']
+    city = data['name']
+    country = inv_country_codes[data['sys']['country']]
+    description = data['weather'][0]['description']
+    feels_like = data['main']['feels_like']
+    temp = data['main']['temp']
+    temp_min = data['main']['temp_min']
+    temp_max = data['main']['temp_max']
+    humidity = data['main']['humidity']
+    wind = data['wind']['speed']
+    lat = data['coord']['lat']
+    lon = data['coord']['lon']
+
+
+    url = weather_img_url.replace('CODE',code)
+    e = discord.Embed(title=f'Weather in {city}, {country}', color=discord.Color.red())
+    e.set_image(url=url)
+    e.add_field(name='Weather Decription', value=description, inline=True)
+    e.add_field(name='Coords', value=f'lat: {lat}, lon: {lon}')
+    e.add_field(name="Temperature", value=f"Temp: {temp} ℃\nMax Temp: {temp_max} ℃\nMin Temp: {temp_min} ℃\nFeels Like: {feels_like} ℃", inline=False)
+    e.add_field(name='Wind Speed', value=f'Wind Speed: {wind} km/hour', inline=True)
+    e.add_field(name='Humidity', value=f'Humidity: {humidity} %', inline=True)
+    return e
+        
+
 @client.event
 async def on_message(msg):
-    global data
-    msg_content = msg.content.lower()
+    global data, trivia_asked, question_token
+    if msg.content.isdigit():
+        msg_content = msg.content
+    else:
+        msg_content = msg.content.lower()[4:].strip()
     param = None
-    if msg_content.startswith('ado '):
-        print(msg_content)
-        msg_content = msg_content[4:].strip()
+    msg_to_send = None
+    e = None
+
+    if msg.content.lower().startswith('ado '):
+        print(msg.content.lower())
 
         if msg_content == 'help':
-            msg_to_send = help_msg
+            e = help_msg
+            
+        elif msg_content == 'ping':
+            msg_to_send = 'pong'
+        
+        elif msg_content == 'help weather':
+            msg_to_send = help_weather_msg # todo
+            print('test')
+
+        elif msg_content.startswith('w'):
+            error_msg = 'please use "ado help weather" to see commands for weather'
+            error_msg2 = 'Please enter a valid city. If you are sure the city/provence is an actual place please report problem to <@!739167113880535191>'
+            try:
+                city, country = msg_content[1:].split(',')
+                city, country = city.strip(), country.strip()
+                print(city, country)
+            except ValueError:
+                msg_to_send = error_msg
+            
+            else:
+                if len(city) > 3: #means its a city name
+                    city_code_lst = get_city_code(city, country)
+                    if city_code_lst:
+                        city_code = city_code_lst[0]['id']
+                        data = get_data(weather_url, data={'appid': weather_token,
+                                                    'id': city_code,
+                                                    'units': 'metric'})
+                        e = get_weather_msg(data)
+                    else:
+                        msg_to_send = error_msg2
+
+                elif len(city) == 3: # means its a zip code prefix
+                    country_code = get_country_code(country).lower()
+                    data = get_data(weather_url, data={'appid': weather_token,
+                                                        'units': 'metric',
+                                                        'zip': f'{city},{country_code}'})
+                    print(data)
+                    e = get_weather_msg(data)
+                        
+                else:
+                    msg_to_send = f'Sorry city/provence was not found. {error_msg2}'
+                
+
+        elif msg_content == 'restart':
+            try:
+                await msg.channel.send('Restarting Ado Bot')
+            except discord.errors.HTTPException:
+                print('network error')
+                
+            print('restarting \n\n\n\n')
+            os.system('bash startup.sh')
+                
+
 
         elif msg_content == 'die':
             msg_to_send = f'ill kill you first {msg.author.mention}'
+        elif msg_content == 'sad':
+            msg_to_send = 'ado is sad u are using dank memer instead of him'
 
         elif 'gay' in msg_content and 'is' in msg_content:
-            if '<@!739167113880535191>' in msg_content:
-                msg_to_send = 'no he isn\'t'
-            elif '<@!389072296805007380>' in msg_content:
-                msg_to_send = 'yes he is'
+            msg_to_send = random.choice(['I\'m not sure', 'yes he is', 'no he isn\'t', 'definitly', 'I mean like, duh', 'Naa', 'no he isn\'t, you are', 'no way'])
+        
+        elif msg_content.startswith('kill'):
+            user_id = msg_content[5:] 
+
+            if len(user_id) == 0:
+                msg_to_send = 'You didn\'t mention who you wanted to kill. I really thought humans were smarter than this'
             else:
-                msg_to_send = random.choice(['yes he is', 'no he isn\'t'])
+                msg_to_send = f'{msg.author.mention}, you just killed {user_id}'
 
         elif msg_content == 'reveal solution' or msg_content == 'reveal answer' or msg_content == 'show answer' or msg_content == 'show solution' or msg_content == 'reveal' or msg_content == 'show':
-            msg_to_send = f'The solution is {data[1]}'
+            if trivia_asked:
+                msg_to_send = f'The solution is {data[1]}'
+                trivia_asked = False
+            else:
+                msg_to_send = 'please ask for a new trivia question before getting its answer'
 
         elif msg_content == 'random cat fact' or msg_content == 'rcf':
             data = get_data('https://catfact.ninja/fact')
@@ -129,9 +281,41 @@ async def on_message(msg):
             msg_to_send = data['fact']
 
         elif msg_content == 'random fact' or msg_content == 'rf':
-            data = get_data('https://useless-facts.sameerkumar.website/api')
+            data = get_data(get_random_question_url())
             print(data)
-            msg_to_send = data['data']
+            try: 
+                msg_to_send = data['data']
+            except KeyError:
+                msg_to_send = data['text']
+
+        elif msg_content == 'bad joke' or msg_content == 'bj':
+            data = get_data('https://official-joke-api.appspot.com/random_joke')
+            print(data)
+            e = discord.Embed(title=f'Bad {(data["type"]).capitalize()} Joke', color=discord.Color.red())
+            e.add_field(name='setup', value=data['setup'], inline=False)
+            e.add_field(name='punchline', value=data['punchline'], inline=False)
+            
+
+        elif msg_content == 'random trivia' or msg_content == 'rt':
+            param = random.choice(['easy','medium','hard'])
+            subject = random.choice(list(subject_codes.keys()))
+            
+            if param:
+                res = get_data(base_url, data={'difficulty':param,
+                                                'category':subject_codes[subject],
+                                                'token': question_token})
+                print(res['response_code'])
+                if res['response_code'] !=0:
+                    res = handel_token_error(res)
+                    question_token = res['token']
+
+                res = get_data(base_url, data={'difficulty':param,
+                                                'category':subject_codes[subject],
+                                                'token': question_token})
+                print(res)
+                data = process_data(res)
+                msg_to_send = f'SUBJECT: {subject}\nDIFFICULTY: {param}\n\n{data[0]} \navailable answers: {data[2]}'
+                trivia_asked = True
 
         elif msg_content.split()[0] in subject_codes.keys():
             subject = msg_content.split()[0]
@@ -144,25 +328,22 @@ async def on_message(msg):
                 param = 'hard'
 
             if param:
-                res = get_data(base_url, param, subject_codes[subject])
-
-                while res['response_code'] != 0:
-                    print(f'error code {res["response_code"]}')
-                    if res['response_code'] == 4:
-                        await msg.channel.send('resetting token')
-                        try:
-                            requests.get(f'https://opentdb.com/api_token.php?command=reset&token={question_token}')
-                        except Exception as exp:
-                            return f'Error retrieving data ({exp})'
-
-                    elif res['response_code'] == 3:
-                        res = requests.get(token_url)
-                        question_token = res.json()['token']
-
-                    res = get_data(base_url, param, subject_codes[subject])
-
+                res = get_data(base_url, data={'difficulty':param,
+                                                'category':subject_codes[subject],
+                                                'token': question_token})
+                print(res['response_code'])
+                if res['response_code'] !=0:
+                    res = handel_token_error(res)
+                    question_token = res['token']
+                    
+                print(res)
+                res = get_data(base_url, data={'difficulty':param,
+                                                'category':subject_codes[subject],
+                                                'token': question_token})
+                print(res, 'test')
                 data = process_data(res)
                 msg_to_send = f'{data[0]} \navailable answers: {data[2]}'
+                trivia_asked = True
 
             else:
                 msg_to_send = 'Try again and please enter a difficulty level (easy, medium, hard)'
@@ -174,28 +355,34 @@ async def on_message(msg):
             except IndexError as ero:
                 print(f'error {ero}')
             else:
+                if trivia_asked:
+                    if ans in trivia_to_letter.keys():
+                        index = trivia_to_letter[ans]
+                        if solution == data[2][index].lower().strip():
+                            msg_to_send = 'Hurray you got the answer correct'
+                            trivia_asked = False
+                        else:
+                            msg_to_send = 'Sorry you\'re answer is wrong. Don\'t worry you can try again.' \
+                                        'If you want to know what was the correct answer type "ado reveal solution"'
 
-                if ans in trivia_to_letter.keys():
-                    index = trivia_to_letter[ans]
-                    if solution == data[2][index].lower().strip():
+                    elif ans == solution:
                         msg_to_send = 'Hurray you got the answer correct'
+                        trivia_asked = False
                     else:
                         msg_to_send = 'Sorry you\'re answer is wrong. Don\'t worry you can try again.' \
-                                      'If you want to know what was the correct answer type "ado reveal solution"'
+                                    'If you want to know what was the correct answer type "ado reveal solution"'
 
-                elif ans == solution:
-                    msg_to_send = 'Hurray you got the answer correct'
                 else:
-                    msg_to_send = 'Sorry you\'re answer is wrong. Don\'t worry you can try again.' \
-                                  'If you want to know what was the correct answer type "ado reveal solution"'
+                    msg_to_send = 'please ask for a new trivia question before you answer'
 
-        try:
-            await msg.channel.send(msg_to_send)
-        except discord.errors.HTTPException:
-            pass
+        if msg_to_send is not None or e is not None:
+            try:
+                await msg.channel.send(msg_to_send, embed=e)
+            except discord.errors.HTTPException:
+                print('error sending msg')
 
 
-# client.loop.create_task(send_rf(787359133590355968))  # young prose server
+#client.loop.create_task(send_rf(787359133590355968))  # young prose server
 # client.loop.create_task(send_rf(795912942678704128))  # grade 10 serve
-client.loop.create_task(keep_token_active())
+
 client.run(token)
