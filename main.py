fbb98ae189dbbdc2657d094344215a6938569151
adam-kabbara <62020687+adam-kabbara@users.noreply.github.com>
2021-02-11 16:51:34 -0500

    added point system

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ from help_msg import help_msg, help_weather_msg
 import json
 import tqdm
 import private_data
-import aiohttp
+from collections import defaultdict
+import copy
+from aiohttp import ClientSession
 from io import BytesIO
 
 token = private_data.ids['discord']
@@ -16,15 +18,30 @@ weather_token = private_data.ids['weather']
 
 client = discord.Client()
 base_url = 'https://opentdb.com/api.php?amount=1&encode=base64'
-data = ()
 token_url = "https://opentdb.com/api_token.php?command=request"
 weather_url = "http://api.openweathermap.org/data/2.5/weather?"
 weather_img_url = "http://openweathermap.org/img/wn/CODE@2x.png"
 res = requests.get(token_url)
 question_token = res.json()['token']
 trivia_asked = False
+trivia_random = None
+difficulty = None
+data = ()
 
 trivia_to_letter = {'a': 0, 'b': 1, 'c': 2, 'd': 3}
+loss_msgs = ['Sorry you\'re answer is wrong. Don\'t worry you can try again.', 
+            'I actually thought humans were smarter than that',
+            'damn you have -100 IQ... congrats',
+            'dumbass thats the wrong answer',
+            'are you fricking kidding me? That is completely wrong',
+            'are you sure you are not an ape. That was the wrong answer']
+
+win_msgs = ['Hurray you got the answer correct', 
+            'Finally a smart one. That was the correct answer',
+            'Damn ur good at this',
+            'Damn that was the correct answer',
+            'You\'re going to outsmart me. That was the correct answer',
+            'Are you sure you\'re not cheating? That was an epic answer']
 
 subject_codes = {
     'math': 19,
@@ -55,6 +72,12 @@ def get_data(url, data={}):
         print(f'Error retrieving data ({exp})')
     else:
         return res.json()
+
+async def get_data(url, data={}):
+    async with ClientSession() as session:
+        async with session.get(url, params = data) as response:
+            response = await response.json()
+            return response
 
 
 def process_data(res):
@@ -100,7 +123,75 @@ def get_random_question_url():
     url = random.choice(['https://uselessfacts.jsph.pl/random.json?language=en', 'https://useless-facts.sameerkumar.website/api'])
     return url
 
-print('running')
+def get_points(user, channel):
+    dd = defaultdict(float)
+    user = str(user)
+    int_dict = dict()
+    with open ('points.json') as f:
+        points = json.load(f)
+    points = points[str(channel)]
+    dd.update(points)
+    for k, v in dd.items():
+        int_dict[k] = int(v)
+    try:
+        return int_dict[user]
+    except KeyError:
+        return 0
+
+def update_points(dictionary, channel):
+    dd = defaultdict(float)
+    main_dict = dict()
+    dictionary = {str(k): v for k, v in  dictionary.items()}
+    with open ('points.json') as f:
+        points = json.load(f)
+
+    points.setdefault(str(channel), {})
+
+    other = copy.deepcopy(points)
+    del other[str(channel)]
+    main_dict.update(other)
+
+    points = points[str(channel)]
+    dd.update(points)
+    for key, value in dictionary.items():
+        dd[key] += value
+    
+    main_dict[str(channel)] = dd
+    with open('points.json', 'w') as f:
+        json.dump(main_dict, f)
+
+def get_top_points(channel):
+    with open ('points.json') as f:
+        points = json.load(f)
+    
+    points.setdefault(str(channel), {})
+    points = points[str(channel)]
+    top = []
+    int_dict = dict()
+
+    for k, v in points.items():
+        int_dict[k] = int(v)
+    
+    sorted_keys = sorted(int_dict, key=int_dict.get, reverse=True)
+    for i, k in enumerate(sorted_keys):
+        if i<=4:
+            top.append((k, int_dict[k]))
+        else:
+            break 
+    return top
+
+def add_points(difficulty, trivia_random, msg):
+    if trivia_random:
+        points_to_add = 10
+    else:
+        if difficulty == 'easy':
+            points_to_add = random.randint(1, 3)
+        elif difficulty == 'medium':
+            points_to_add = random.randint(3, 5)
+        else:
+            points_to_add = random.randint(6, 8)
+    print(points_to_add)
+    update_points({msg.author.id: points_to_add}, msg.guild.id)
 
 
 def handel_token_error(res):
@@ -109,7 +200,6 @@ def handel_token_error(res):
         if res['response_code'] == 4:
             try:
                 res = requests.get(f'https://opentdb.com/api_token.php?command=reset&token={question_token}').json()
-                print(res)
             except Exception as exp:
                 return f'Error retrieving data ({exp})'
 
@@ -117,8 +207,6 @@ def handel_token_error(res):
             print('debug')
             res = (requests.get(token_url)).json()
             question_token = res['token']
-            print(res)
-            print(question_token)
             
     return res
 
@@ -182,30 +270,35 @@ def get_weather_msg(code):
     e.add_field(name='Humidity', value=f'Humidity: {humidity} %', inline=True)
     return e
         
+print('running')
 
 @client.event
 async def on_message(msg):
-    global data, trivia_asked, question_token
+    global trivia_asked, question_token, trivia_random, difficulty, data
     if msg.content.isdigit():
         msg_content = msg.content
     else:
         msg_content = msg.content.lower()[4:].strip()
-    param = None
     msg_to_send = None
     e = None
+     
 
     if msg.content.lower().startswith('ado '):
         print(msg.content.lower())
 
         if msg_content == 'help':
             e = help_msg
+
+        if msg_content == 'test':
+            import time
+            await asyncio.sleep(5)
+            msg_to_send = 'test'
             
         elif msg_content == 'ping':
             msg_to_send = 'pong'
         
         elif msg_content == 'help weather':
             msg_to_send = help_weather_msg # todo
-            print('test')
 
         elif msg_content.startswith('w'):
             error_msg = 'please use "ado help weather" to see commands for weather'
@@ -213,7 +306,6 @@ async def on_message(msg):
             try:
                 city, country = msg_content[1:].split(',')
                 city, country = city.strip(), country.strip()
-                print(city, country)
             except ValueError:
                 msg_to_send = error_msg
             
@@ -222,7 +314,7 @@ async def on_message(msg):
                     city_code_lst = get_city_code(city, country)
                     if city_code_lst:
                         city_code = city_code_lst[0]['id']
-                        data = get_data(weather_url, data={'appid': weather_token,
+                        data = await get_data(weather_url, data={'appid': weather_token,
                                                     'id': city_code,
                                                     'units': 'metric'})
                         e = get_weather_msg(data)
@@ -231,10 +323,9 @@ async def on_message(msg):
 
                 elif len(city) == 3: # means its a zip code prefix
                     country_code = get_country_code(country).lower()
-                    data = get_data(weather_url, data={'appid': weather_token,
+                    data = await get_data(weather_url, data={'appid': weather_token,
                                                         'units': 'metric',
                                                         'zip': f'{city},{country_code}'})
-                    print(data)
                     e = get_weather_msg(data)
                         
                 else:
@@ -249,8 +340,24 @@ async def on_message(msg):
                 
             print('restarting \n\n\n\n')
             os.system('bash startup.sh')
-                
 
+        elif msg_content.startswith('points'):
+            if msg_content == "points":
+                points = get_points(msg.author.id, msg.guild.id)
+                msg_to_send = f'you have {points} points'
+
+            elif len(msg.mentions) == 1:
+                user = msg.mentions[0].id
+                points = get_points(user, msg.guild.id)
+                msg_to_send = f'{msg.mentions[0].mention} has {points} points'
+            
+            elif msg_content.split()[1] == 'top':
+                top = get_top_points(msg.guild.id)
+                e = discord.Embed(title='Top points', color=discord.Color.red())
+                for k, v in top:
+                    user = await client.fetch_user(k)
+                    e.add_field(name=f'{user.name}', value=v, inline=False)
+                    
 
         elif msg_content == 'die':
             msg_to_send = f'ill kill you first {msg.author.mention}'
@@ -276,59 +383,62 @@ async def on_message(msg):
                 msg_to_send = 'please ask for a new trivia question before getting its answer'
 
         elif msg_content == 'random cat fact' or msg_content == 'rcf':
-            data = get_data('https://catfact.ninja/fact')
+            data = await get_data('https://catfact.ninja/fact')
             print(data)
             msg_to_send = data['fact']
 
         elif msg_content == 'random fact' or msg_content == 'rf':
-            data = get_data(get_random_question_url())
+            data = await get_data(get_random_question_url())
             print(data)
             try: 
                 msg_to_send = data['data']
             except KeyError:
                 msg_to_send = data['text']
 
-        elif msg_content == 'bad joke' or msg_content == 'bj':
-            data = get_data('https://official-joke-api.appspot.com/random_joke')
-            print(data)
-            e = discord.Embed(title=f'Bad {(data["type"]).capitalize()} Joke', color=discord.Color.red())
-            e.add_field(name='setup', value=data['setup'], inline=False)
-            e.add_field(name='punchline', value=data['punchline'], inline=False)
+        elif msg_content == 'joke':
+            data = await get_data('https://official-joke-api.appspot.com/random_joke')
+            _type = data['type']
+            if _type == 'general':
+                _type = ''
+            print(_type)
+            e = discord.Embed(title=f'Bad {(_type).capitalize()} Joke', color=discord.Color.red())
+            e.add_field(name='Joke', value=f"{data['setup']} {data['punchline']}", inline=False)
             
+        elif msg_content == 'bj':
+            msg_to_send = 'please use [ado joke]'
+
 
         elif msg_content == 'random trivia' or msg_content == 'rt':
-            param = random.choice(['easy','medium','hard'])
+            difficulty = random.choice(['easy','medium','hard'])
             subject = random.choice(list(subject_codes.keys()))
             
-            if param:
-                res = get_data(base_url, data={'difficulty':param,
-                                                'category':subject_codes[subject],
-                                                'token': question_token})
-                print(res['response_code'])
-                if res['response_code'] !=0:
-                    res = handel_token_error(res)
-                    question_token = res['token']
+            res = await get_data(base_url, data={'difficulty':difficulty,
+                                            'category':subject_codes[subject],
+                                            'token': question_token})
+            if res['response_code'] !=0:
+                res = handel_token_error(res)
+                question_token = res['token']
 
-                res = get_data(base_url, data={'difficulty':param,
-                                                'category':subject_codes[subject],
-                                                'token': question_token})
-                print(res)
-                data = process_data(res)
-                msg_to_send = f'SUBJECT: {subject}\nDIFFICULTY: {param}\n\n{data[0]} \navailable answers: {data[2]}'
-                trivia_asked = True
+            res = await get_data(base_url, data={'difficulty':difficulty,
+                                            'category':subject_codes[subject],
+                                            'token': question_token})
+            data = process_data(res)
+            msg_to_send = f'SUBJECT: {subject}\nDIFFICULTY: {difficulty}\n\n{data[0]} \navailable answers: {data[2]}'
+            trivia_asked = True
+            trivia_random = True
 
         elif msg_content.split()[0] in subject_codes.keys():
             subject = msg_content.split()[0]
 
             if 'easy' in msg_content:
-                param = 'easy'
+                difficulty = 'easy'
             elif 'medium' in msg_content:
-                param = 'medium'
+                difficulty = 'medium'
             elif 'hard' in msg_content:
-                param = 'hard'
+                difficulty = 'hard'
 
-            if param:
-                res = get_data(base_url, data={'difficulty':param,
+            if difficulty:
+                res = await get_data(base_url, data={'difficulty':difficulty,
                                                 'category':subject_codes[subject],
                                                 'token': question_token})
                 print(res['response_code'])
@@ -336,14 +446,13 @@ async def on_message(msg):
                     res = handel_token_error(res)
                     question_token = res['token']
                     
-                print(res)
-                res = get_data(base_url, data={'difficulty':param,
+                res = await get_data(base_url, data={'difficulty':difficulty,
                                                 'category':subject_codes[subject],
                                                 'token': question_token})
-                print(res, 'test')
                 data = process_data(res)
                 msg_to_send = f'{data[0]} \navailable answers: {data[2]}'
                 trivia_asked = True
+                trivia_random = False
 
             else:
                 msg_to_send = 'Try again and please enter a difficulty level (easy, medium, hard)'
@@ -359,18 +468,20 @@ async def on_message(msg):
                     if ans in trivia_to_letter.keys():
                         index = trivia_to_letter[ans]
                         if solution == data[2][index].lower().strip():
-                            msg_to_send = 'Hurray you got the answer correct'
+                            msg_to_send = random.choice(win_msgs)
                             trivia_asked = False
+                            add_points(difficulty, trivia_random, msg)
+                            trivia_random = None
                         else:
-                            msg_to_send = 'Sorry you\'re answer is wrong. Don\'t worry you can try again.' \
-                                        'If you want to know what was the correct answer type "ado reveal solution"'
+                            msg_to_send = random.choice(loss_msgs)
 
                     elif ans == solution:
-                        msg_to_send = 'Hurray you got the answer correct'
+                        msg_to_send = random.choice(win_msgs)
                         trivia_asked = False
+                        add_points(difficulty, trivia_random, msg)          
+                        trivia_random = None
                     else:
-                        msg_to_send = 'Sorry you\'re answer is wrong. Don\'t worry you can try again.' \
-                                    'If you want to know what was the correct answer type "ado reveal solution"'
+                        msg_to_send = random.choice(loss_msgs)
 
                 else:
                     msg_to_send = 'please ask for a new trivia question before you answer'
@@ -381,8 +492,5 @@ async def on_message(msg):
             except discord.errors.HTTPException:
                 print('error sending msg')
 
-
-#client.loop.create_task(send_rf(787359133590355968))  # young prose server
-# client.loop.create_task(send_rf(795912942678704128))  # grade 10 serve
 
 client.run(token)
